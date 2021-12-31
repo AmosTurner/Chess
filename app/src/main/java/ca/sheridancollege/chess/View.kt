e7f1b9e6d8ea2d4dbc3fc7d119d78084a9931048
AmosTurner <turneamo@sheridan.desire2learn.com>
2021-12-30 23:05:21 -0500

    refactored the algorithm for drawing the chessboard

--- app/src/main/java/ca/sheridancollege/chess/View.kt
+++ app/src/main/java/ca/sheridancollege/chess/View.kt
@@ -16,37 +16,14 @@ class View(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
         // Paint specifies what to draw
         val paint = Paint()
 
-        for (j in 0..3) {
-            for (i in 0..3) {
-                paint.color = Color.LTGRAY
+        for (i in 0..7) {
+            for (j in 0..7) {
+                paint.color = if ((i + j) % 2 == 1) Color.DKGRAY else Color.LTGRAY
                 canvas?.drawRect(
-                    xCoordinate + 2 * i * cellSide,
-                    yCoordinate + 2 * j * cellSide,
-                    xCoordinate + (2 * i + 1) * cellSide,
-                    yCoordinate + (2 * j + 1) * cellSide,
-                    paint
-                )
-                canvas?.drawRect(
-                    xCoordinate + (2 * i + 1) * cellSide,
-                    yCoordinate + (2 * j + 1) * cellSide,
-                    xCoordinate + (2 * i + 2) * cellSide,
-                    yCoordinate + (2 * j + 2) * cellSide,
-                    paint
-                )
-
-                paint.color = Color.DKGRAY
-                canvas?.drawRect(
-                    xCoordinate + (2 * i + 1) * cellSide,
-                    yCoordinate + 2 * j * cellSide,
-                    xCoordinate + (2 * i + 2) * cellSide,
-                    yCoordinate + (2 * j + 1) * cellSide,
-                    paint
-                )
-                canvas?.drawRect(
-                    xCoordinate + (2 * i) * cellSide,
-                    yCoordinate + (2 * j + 1) * cellSide,
-                    xCoordinate + (2 * i + 1) * cellSide,
-                    yCoordinate + (2 * j + 2) * cellSide,
+                    xCoordinate + i * cellSide,
+                    yCoordinate + j * cellSide,
+                    xCoordinate + (i + 1) * cellSide,
+                    yCoordinate + (j + 1) * cellSide,
                     paint
                 )
             }
